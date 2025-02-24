117abe1626ba9679fbb71e2061da490f27a279d5
Irina <ouaaey@gmail.com>
2025-02-24 22:16:01 +0400

    2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
+
     @Autowired
     @Qualifier("dog")
     private Animal animal;
+
     @Autowired
     private Timer timer;
 
@@ -21,7 +23,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -3,15 +3,13 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Timer {
+public class Timer implements TimerInterface {
 
-    private Long nanoTime = System.nanoTime();
+    private long nanoTime = System.nanoTime();
 
-    public void start() {
-        nanoTime = System.nanoTime();
-    }
-
+    @Override
     public Long getTime() {
         return nanoTime;
     }
+
 }
